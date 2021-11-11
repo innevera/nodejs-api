1dc42d943c9de1a2753f4016dc192534e912f4f0
Halil Bilgin <hlilbilgin@gmail.com>
2021-11-12 00:10:53 +0300

    create models

--- api/v1/models/User.js
+++ api/v1/models/User.js
@@ -1,15 +1,21 @@
-const Mongoose = require('mongoose');
+const Mongoose = require("mongoose");
 
-const UserSchema = new Mongoose.Schema({
+const UserSchema = new Mongoose.Schema(
+  {
+    id: Number, // new => Document id'si çok uzun olduğu için diğer dokumanların boyutunu arttıracak
     name: String,
     surname: String,
     username: String,
     email: String,
     password: String,
-    image: String,
+    image: String, // dosyaların listelendiği bir doküman olursa id olacak.
     role: Number,
-    active: Boolean,
-    deleted: Boolean
-}, { timestamps: true, versionKey: false });
+    locale: Number, // new => kullanıcı ülkesi/dili kodu
+    active: Boolean, // active ve delete status olarak tek bir alana dönüştürüp, statusType için ayrı bir doküman olabilir.
+    deleted: Boolean, //
+    // status: { statusType: Number, updatedAt: Date},
+  },
+  { timestamps: true, versionKey: false }
+);
 
-module.exports = Mongoose.model('users', UserSchema);
+module.exports = Mongoose.model("users", UserSchema);
