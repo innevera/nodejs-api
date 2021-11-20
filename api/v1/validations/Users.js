3b3ebf3f6b477ac00fb7fada772d95eeb92b58e5
Halil Bilgin <hlilbilgin@gmail.com>
2021-11-20 14:10:17 +0300

    Update Users.js

--- api/v1/validations/Users.js
+++ api/v1/validations/Users.js
@@ -7,7 +7,7 @@ const validation = {
         name: Joi.string().required().min(2),
         surname: Joi.string().required().min(2),
         username: Joi.string().required().min(4),
-        password: Joi.string().required().min(6),
+        password: Joi.string().min(6),
         email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: false } }),
         phone: Joi.string(), // only TR phones
         media: Joi.string(),
